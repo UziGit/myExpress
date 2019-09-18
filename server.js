fb6aaef820f185614e04c975a624e4fee4837814
LiWei <3109554662@qq.com>
2019-09-18 11:00:33 +0800

    '中间件'

--- server.js
+++ server.js
@@ -3,8 +3,29 @@ const express = require('express');
 const server = express();
 const cookieParser = require('cookie-parser');
 
+//定义一个中间件
+const myTime = (num) => {
+    return (req,res,next) => {
+        let dates = new Date();
+        let year = dates.getFullYear();
+        let mon = dates.getMonth() + 1;
+        let day = dates.getDate(); 
+        if(num == 1){
+            req.requestTime = `${year}年`;
+            next();
+        }else if (num == 2) {
+            req.requestTime = `${year}年  ${mon}月`;
+            next();
+        }else if(num == 3){
+            req.requestTime = `${year}年  ${mon}月  ${day}日`;
+            next();
+        }
+        
+    }
+}
 server.use(express.json());
 server.use(express.urlencoded({ extended:true }));
+server.use(myTime(3))
 //设置一个中间件给req加上一个cookies 的属性
 server.use(cookieParser());
 //去处理路由
@@ -30,6 +51,10 @@ server.get('/getCookie',(req,res) => {
     res.send('获取成功');
 });
 
+server.get('/reqs',(req,res) => {
+    console.log(req.requestTime);
+    res.send('添加成功');
+})
 //监听一个端口号
 server.listen(3000,() => {
     console.log('服务已经启动，地址是：http://127.0.0.1:3000');
